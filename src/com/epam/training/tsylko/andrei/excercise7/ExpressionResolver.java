3591dedf5abfb8be7969b9e9d01b92b7be58acf6
Andrei Tsylko <mill9r@gmail.com>
2017-06-22 10:53:56 +0300

    version 1.1

--- src/com/epam/training/tsylko/andrei/excercise7/ExpressionResolver.java
+++ src/com/epam/training/tsylko/andrei/excercise7/ExpressionResolver.java
@@ -1,10 +1,15 @@
 package com.epam.training.tsylko.andrei.excercise7;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ExpressionResolver {
-    public static void showResult(int a, int b, int h) {
+    public static Map resolveExpression(double a, double b, double h) {
+        Map<Double,Double> result = new HashMap<>();
         for (; a < b; a += h) {
-            System.out.println(a + "\t" + (Math.pow(Math.sin(a), 2.0) - Math.cos(2.0 * a)));
+            result.put(a,(Math.pow(Math.sin(a), 2.0) - Math.cos(2.0 * a)));
         }
+        return result;
     }
 }
--- src/com/epam/training/tsylko/andrei/excercise7/Test.java
+++ src/com/epam/training/tsylko/andrei/excercise7/Test.java
@@ -2,19 +2,22 @@ package com.epam.training.tsylko.andrei.excercise7;
 
 import com.epam.training.tsylko.andrei.util.Util;
 
+import java.util.Map;
+
 public class Test {
 
-	public static final String CHECKER = "^[+,-]?[0-9]{1,3}";
+	public static final String CHECKER = "^[+,-]?d{1,3}";
 
 	public static void main(String[] args) {
+		args.toString();
 		if (args.length == 3) {
 			if (Util.checkInputNumbers(args[0], CHECKER) && Util.checkInputNumbers(args[1], CHECKER) && Util.checkInputNumbers(args[3], CHECKER)) {
-				int a = Integer.parseInt(args[0]);
-				int b = Integer.parseInt(args[1]);
-				int h = Integer.parseInt(args[2]);
+				double a = Double.parseDouble(args[0]);
+				double b = Double.parseDouble(args[1]);
+				double h = Double.parseDouble(args[2]);
 
-				ExpressionResolver.showResult(a, b, h);
-
+			Map<Double,Double> result = ExpressionResolver.resolveExpression(a, b, h);
+			result.forEach((k,v)-> System.out.println(k + "\t : " + v));
 			} else {
 				System.out.println("Check entered numbers");
 			}
